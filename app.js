1c2bb12dd444e5395c1bfe5652a15918b83d63ff
unknown <unknown@example.com>
2020-03-23 10:59:11 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+App({
+  onLaunch () {
+    wx.cloud.init({
+      env: 'envid',
+      traceUser: true,
+    })
+    wx.getSystemInfo({
+      success: (res) => {
+        this.globalData.systeminfo = res
+        this.globalData.isIPhoneX = /iphonex/gi.test(res.model.replace(/\s+/, ''))
+      },
+    })
+  },
+  globalData: {
+    // 是否保持常亮，离开小程序失效
+    keepscreenon:false,
+    systeminfo: {},
+    isIPhoneX: false,
+    key: 'cecd1023239e475bad5f98ea1c504028',
+    weatherIconUrl: 'https://cdn.heweather.com/cond_icon/',
+    requestUrl: {
+      weather: 'https://free-api.heweather.com/s6/weather',
+      hourly: 'https://free-api.heweather.com/s6/weather/hourly',
+    },
+  },
+})
